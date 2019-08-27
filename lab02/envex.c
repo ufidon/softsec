b70333aa2d6a8f179abbb1f2d2876a7cd68c1516
ufidon <ufidon@gmail.com>
2019-08-27 10:55:42 -0500

    lab02 added

---
+++ lab02/envex.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+extern char **environ;
+
+int main()
+{
+    char *argv[2];
+    argv[0] = "/usr/bin/env";
+    argv[1] = NULL;
+    execve("/usr/bin/env", argv, NULL);
+    return 0;
+}
